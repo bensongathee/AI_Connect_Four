6f560d3096827785a1d7cc622e99b9f7db0a5506
bensongathee <benson7484@gmail.com>
2019-04-01 05:10:19 -0400

    updated files

--- AI_Connect_Four/src/Node.java
+++ AI_Connect_Four/src/Node.java
@@ -8,49 +8,41 @@ public class Node
 	private Expand expand = new Expand();
 	private int BestMove;
 	
-	public Node(int[][] State)
-	{
+	public Node(int[][] State){
 		this.State= State;
 	}
 	
-	public int[][] GetState()
-	{
+	public int[][] GetState(){
 		return State;
 	}
 	
-	public double GetUtility()
-	{
+	public double GetUtility(){
 		return Utility;
 	}
 	
-	public void SetUtility(double Utility)
-	{
+	public void SetUtility(double Utility){
 		this.Utility = Utility;
 	}
-	public int GetBestMove()
-	{
+	
+	public int GetBestMove(){
 		return BestMove;
 	}
 	
-	public void SetBestMove(int move)
-	{
+	public void SetBestMove(int move){
 		this.BestMove = move;
 	}
+	
 	public List<Node> Expand(boolean Player)
 	{
 		return expand.MakeChildren(State, Player);
 	}
 }
 
-class Expand
-{
-	public ArrayList<Node> MakeChildren(int[][] State, boolean Player) 
-	{
+class Expand {
+	public ArrayList<Node> MakeChildren(int[][] State, boolean Player) {
 		ArrayList<Node> frontier= new ArrayList<Node>();
-		for(int j=0; j < State[0].length; j++) 
-		{
-			if(Problem.Action(State,j))
-			{
+		for(int j=0; j < State[0].length; j++)  {
+			if(Problem.Action(State,j)) {
 				Node node = new Node(AddChild(State,j,Player));
 				node.SetBestMove(j);
 				frontier.add(node);
@@ -58,17 +50,14 @@ class Expand
 		}
 		return frontier;
 	}
-	public int[][] AddChild(int[][] State, int j, boolean Player) 
-	{
+	public int[][] AddChild(int[][] State, int j, boolean Player) {
 		int[][] ChildState = new int[State.length][State[0].length];
 		for(int i=0; i<State.length; i++)
 			for(int k=0; k<State[0].length; k++)
 				ChildState[i][k]= State[i][k];
 		
-		for(int i = (State.length-1); i >= 0; i--) 
-		{
-			if(ChildState[i][j]==0) 
-			{
+		for(int i = (State.length-1); i >= 0; i--) {
+			if(ChildState[i][j]==0) {
 				if(Player == false)
 					ChildState[i][j]=1;
 				else
--- AI_Connect_Four/src/Problem.java
+++ AI_Connect_Four/src/Problem.java
@@ -5,10 +5,8 @@ public class Problem {
 	private static int[][] InitialState;
 	private static Check check = new Check();
 	
-	public Problem(int Case)
-	{
-		switch (Case) 
-		{
+	public Problem(int Case){
+		switch (Case) {
 		case 1:
 			K_game = 3;
 			InitialState = new int[3][3];
@@ -25,21 +23,19 @@ public class Problem {
 			break;
 		}
 	}
-	public static int[][] InitialState()
-	{
+	
+	public static int[][] InitialState(){
 		return InitialState;
 	}
-	public static boolean Action(int[][] State,int j) 
-	{
+	
+	public static boolean Action(int[][] State,int j) {
 		if(check.TestAction(State, j))
 			return true;
 		return false;
 	}
 
-	public static boolean TerminalState(int[][] State) 
-	{
-		if(check.IsWinner(State,K_game))
-		{
+	public static boolean TerminalState(int[][] State) {
+		if(check.IsWinner(State,K_game)) {
 			if (Check.WinnerPlayer == 1)
 				Utility = -1;
 			else
@@ -47,33 +43,30 @@ public class Problem {
 			
 			return true;
 		}
-		else if(check.IsFull(State))
-		{
+		else if(check.IsFull(State)) {
 			Utility = 0;
 			return true;
 		}
 		
 		return false;
 	}
-	public static int Utility()
-	{
+	public static int Utility() {
 		return Utility;
 	}
 	
-	public static Node Result(int[][] GridState, int Column, int Color)
-	{
+	public static Node Result(int[][] GridState, int Column, int Color) {
 		return check.PutaMark(GridState, Column, Color);
 	}
-	public static boolean Draw(int[][] State)
-	{
+	
+	public static boolean Draw(int[][] State) {
 		if(check.IsWinner(State, Problem.K_game))
 			return false;
 		else if(check.IsFull(State))
 			return true;
 		return false;
 	}
-	public static double CutOffUtility(int[][] State)
-	{
+	
+	public static double CutOffUtility(int[][] State) {
 		return check.CutOff(State);
 	}
 }
@@ -82,64 +75,53 @@ class Check
 {
 	static int WinnerPlayer; //Change
 	
-	public double CutOff(int[][] State)
-	{
+	public double CutOff(int[][] State) {
 		double Max = 0, Min = 0; 
-		for(int i = 0; i < State.length; i++)
-		{
-			for(int j = 0; j < State[0].length; j++)
-			{
-				if(i != 0)
-				{
+		for(int i = 0; i < State.length; i++) {
+			for(int j = 0; j < State[0].length; j++) {
+				if(i != 0) {
 					if(State[i-1][j] == 1)
 						Min -=1; 
 					else if(State[i-1][j] == 2)
 						Max +=1;
 				}
-				else if(i != (State.length-1))
-				{
+				else if(i != (State.length-1)) {
 					if(State[i+1][j] == 1)
 						Min -=1;
 					else if(State[i+1][j] == 2)
 						Max +=1;
 				}
-				else if(j != 0)
-				{
+				else if(j != 0) {
 					if(State[i][j-1] == 1)
 						Min -=1;
 					else if(State[i][j-1] == 2)
 						Max +=1;
 				}
-				else if(j != (State[0].length-1))
-				{
+				else if(j != (State[0].length-1)) {
 					if(State[i][j+1] == 1)
 						Min -=1;
 					else if(State[i][j+1] == 2)
 						Max +=1;
 				}
-				else if(i != 0 && j != 0)
-				{
+				else if(i != 0 && j != 0) {
 					if(State[i-1][j-1] == 1)
 						Min -=1;
 					else if(State[i-1][j-1] == 2)
 						Max +=1;
 				}
-				else if(i != (State.length-1) && j != (State[0].length-1))
-				{
+				else if(i != (State.length-1) && j != (State[0].length-1)) {
 					if(State[i+1][j+1] == 1)
 						Min -=1;
 					else if(State[i+1][j+1] == 2)
 						Max +=1;
 				}
-				else if(i != 0 && j != (State[0].length-1))
-				{
+				else if(i != 0 && j != (State[0].length-1)) {
 					if(State[i-1][j+1] == 1)
 						Min -=1;
 					else if(State[i-1][j+1] == 2)
 						Max +=1;
 				}
-				else if(i != State.length-1 && j != 0)
-				{
+				else if(i != State.length-1 && j != 0) {
 					if(State[i+1][j-1] == 1)
 						Min -=1;
 					else if(State[i+1][j-1] == 2)
@@ -151,47 +133,37 @@ class Check
 		return average;
 	}
 	
-	public Node PutaMark(int[][] State, int Column, int Color)
-	{
-		for(int i = (State.length-1); i >= 0; i--) 
-		{
-			if(State[i][Column]==0) 
-			{
+	public Node PutaMark(int[][] State, int Column, int Color){
+		for(int i = (State.length-1); i >= 0; i--) {
+			if(State[i][Column]==0) {
 				State[i][Column] = Color;
 				break;
 			}	
 		}
 		return new Node(State);
 	}
-	public boolean TestAction(int[][] State, int j) 
-	{
+	public boolean TestAction(int[][] State, int j) {
 		if(State[0][j]==0)
 			return true;
 		return false;
 	}
 	
-	public Boolean IsFull(int[][] State) 
-	{		
-		for(int j=0; j<State[0].length; j++) 
-		{
+	public Boolean IsFull(int[][] State) {		
+		for(int j=0; j<State[0].length; j++) {
 			if(State[0][j]==0)
 				return false;
 		}
 		return true;
 	}
-	public boolean IsWinner(int[][] State, int k_game)
-	{
+	public boolean IsWinner(int[][] State, int k_game) {
 		int i,j;
 		
-		for(i=0; i<State.length; i++) 
-		{
-			for(j=0; j<=State[0].length-k_game; j++) 
-			{
+		for(i=0; i<State.length; i++) {
+			for(j=0; j<=State[0].length-k_game; j++) {
 				if(State[i][j] == State[i][j+1] &&
 				   State[i][j] == State[i][j+2] &&
 				   State[i][j] == State[i][j+k_game-1] &&
-				   State[i][j]!=0) 
-				{
+				   State[i][j]!=0) {
 					if(State[i][j] == 1)
 						WinnerPlayer = 1;
 					else 
@@ -201,15 +173,12 @@ class Check
 			}
 		}
 		
-		for(i=0; i<=State.length-k_game; i++) 
-		{
-			for(j=0; j<State[0].length; j++) 
-			{
+		for(i=0; i<=State.length-k_game; i++) {
+			for(j=0; j<State[0].length; j++) {
 				if(State[i][j] == State[i+1][j] &&
 				   State[i][j] == State[i+2][j] &&
 				   State[i][j] == State[i+k_game-1][j] &&
-				   State[i][j]!=0)
-				{
+				   State[i][j]!=0) {
 					if(State[i][j] == 1)
 						WinnerPlayer = 1;
 					else 
@@ -219,15 +188,12 @@ class Check
 			}
 		}
 		
-		for(i=0; i<=State.length-k_game; i++) 
-		{
-			for(j=0; j<=State[0].length-k_game; j++) 
-			{
+		for(i=0; i<=State.length-k_game; i++) {
+			for(j=0; j<=State[0].length-k_game; j++) {
 				if(State[i][j] == State[i+1][j+1] &&
 				   State[i][j] == State[i+2][j+2] &&
 				   State[i][j] == State[i+k_game-1][j+k_game-1] &&
-				   State[i][j]!=0)
-				{
+				   State[i][j]!=0) {
 					if(State[i][j] == 1)
 						WinnerPlayer = 1;
 					else 
@@ -237,15 +203,12 @@ class Check
 			}
 		}
 		
-		for(i=State.length-1; i>=k_game-1; i--) 
-		{
-			for(j=0; j<=State[0].length-k_game; j++) 
-			{
+		for(i=State.length-1; i>=k_game-1; i--) {
+			for(j=0; j<=State[0].length-k_game; j++) {
 				if(State[i][j] == State[i-1][j+1] &&
 				   State[i][j] == State[i-2][j+2] &&
 				   State[i][j] == State[i-(k_game-1)][j+k_game-1] &&
-				   State[i][j]!=0)
-				{
+				   State[i][j]!=0) {
 					if(State[i][j] == 1)
 						WinnerPlayer = 1;
 					else 
